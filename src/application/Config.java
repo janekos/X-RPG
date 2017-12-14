76e6a5e9bfa0cf1dad1119ca65f191d6c673966c
Janek K <janek.kossinski@gmail.com>
2017-12-14 20:03:56 +0200

    map and tile fix

--- src/application/Config.java
+++ src/application/Config.java
@@ -2,27 +2,18 @@ package application;
 
 public class Config {
 	public Tile[][] map1 = {
-		[ , , , , , , , , , ],
-		[ , , , , , , , , , ],
-		[ , , , , , , , , , ],
-		[ , , , , , , , , , ],
-		[ , , , , , , , , , ],
-		[ , , , , , , , , , ],
-		[ , , , , , , , , , ],
-		[ , , , , , , , , , ],
-		[ , , , , , , , , , ],
-		[ , , , , , , , , , ]	
+		{Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT },
+		{Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT },
+		{Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT },
+		{Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT },
+		{Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT },
+		{Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT },
+		{Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT },
+		{Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT },
+		{Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT },
+		{Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT, Tile.GRASS, Tile.DIRT }
 	};
 	public Tile[][] map2 = {
-		[ , , , , , , , , , ],
-		[ , , , , , , , , , ],
-		[ , , , , , , , , , ],
-		[ , , , , , , , , , ],
-		[ , , , , , , , , , ],
-		[ , , , , , , , , , ],
-		[ , , , , , , , , , ],
-		[ , , , , , , , , , ],
-		[ , , , , , , , , , ],
-		[ , , , , , , , , , ]
+		
 	};
 }
--- src/application/Tile.java
+++ src/application/Tile.java
@@ -14,19 +14,19 @@ public class Tile {
 	public String getName() { return name;}
 	public Image getImg() {return img;}
 	
-	public Tile BUSH_GRASS_TILE = new Tile("BUSH_GRASS_TILE", "tiles/twoD/BUSH_GRASS_TILE.png", false, false);
-	public Tile DIRT = new Tile("DIRT", "tiles/twoD/DIRT_TILE.png", true, false);
-	public Tile GRASS_DARKER = new Tile("GRASS_DARKER", "tiles/twoD/GRASS_DARKER_TILE.png", true, false);
-	public Tile GRASS = new Tile("GRASS", "tiles/twoD/GRASS_TILE.png", true, false);
-	public Tile HOUSE_LOWER_LEFT = new Tile("HOUSE_LOWER_LEFT", "tiles/twoD/HOUSE_LOWER_LEFT_TILE.png", false, true);
-	public Tile HOUSE_LOWER_RIGHT = new Tile("HOUSE_LOWER_RIGHT", "tiles/twoD/HOUSE_LOWER_RIGHT_TILE.png", false, true);
-	public Tile HOUSE_UPPER_LEFT = new Tile("HOUSE_UPPER_LEFT", "tiles/twoD/HOUSE_UPPER_LEFT_TILE.png", false, true);
-	public Tile HOUSE_UPPER_RIGHT = new Tile("HOUSE_UPPER_RIGHT", "tiles/twoD/HOUSE_UPPER_RIGHT_TILE.png", false, true);
-	public Tile METAL_FENCE_DIRT = new Tile("METAL_FENCE_DIRT", "tiles/twoD/METAL_FENCE_DIRT_TILE.png", false, false);
-	public Tile METAL_FENCE_GRASS = new Tile("METAL_FENCE_GRASS", "tiles/twoD/METAL_FENCE_GRASS_TILE.png", false, false);
-	public Tile PAVEMENT = new Tile("PAVEMENT", "tiles/twoD/PAVEMENT_TILE.png", true, false);
-	public Tile ROCK_DIRT = new Tile("ROCK_DIRT", "tiles/twoD/ROCK_DIRT_TILE.png", false, false);
-	public Tile ROCK_GRASS = new Tile("ROCK_GRASS", "tiles/twoD/ROCK_GRASS_TILE.png", false, false);
-	public Tile TREE_GRASS_DARKER = new Tile("TREE_GRASS_DARKER", "tiles/twoD/TREE_GRASS_DARKER_TILE.png", false, false);
-	public Tile TREE_GRASS = new Tile("TREE_GRASS", "tiles/twoD/TREE_GRASS_TILE.png", false, false);
+	public static Tile BUSH_GRASS_TILE = new Tile("BUSH_GRASS_TILE", "tiles/twoD/BUSH_GRASS_TILE.png", false, false);
+	public static Tile DIRT = new Tile("DIRT", "tiles/twoD/DIRT_TILE.png", true, false);
+	public static Tile GRASS_DARKER = new Tile("GRASS_DARKER", "tiles/twoD/GRASS_DARKER_TILE.png", true, false);
+	public static Tile GRASS = new Tile("GRASS", "tiles/twoD/GRASS_TILE.png", true, false);
+	public static Tile HOUSE_LOWER_LEFT = new Tile("HOUSE_LOWER_LEFT", "tiles/twoD/HOUSE_LOWER_LEFT_TILE.png", false, true);
+	public static Tile HOUSE_LOWER_RIGHT = new Tile("HOUSE_LOWER_RIGHT", "tiles/twoD/HOUSE_LOWER_RIGHT_TILE.png", false, true);
+	public static Tile HOUSE_UPPER_LEFT = new Tile("HOUSE_UPPER_LEFT", "tiles/twoD/HOUSE_UPPER_LEFT_TILE.png", false, true);
+	public static Tile HOUSE_UPPER_RIGHT = new Tile("HOUSE_UPPER_RIGHT", "tiles/twoD/HOUSE_UPPER_RIGHT_TILE.png", false, true);
+	public static Tile METAL_FENCE_DIRT = new Tile("METAL_FENCE_DIRT", "tiles/twoD/METAL_FENCE_DIRT_TILE.png", false, false);
+	public static Tile METAL_FENCE_GRASS = new Tile("METAL_FENCE_GRASS", "tiles/twoD/METAL_FENCE_GRASS_TILE.png", false, false);
+	public static Tile PAVEMENT = new Tile("PAVEMENT", "tiles/twoD/PAVEMENT_TILE.png", true, false);
+	public static Tile ROCK_DIRT = new Tile("ROCK_DIRT", "tiles/twoD/ROCK_DIRT_TILE.png", false, false);
+	public static Tile ROCK_GRASS = new Tile("ROCK_GRASS", "tiles/twoD/ROCK_GRASS_TILE.png", false, false);
+	public static Tile TREE_GRASS_DARKER = new Tile("TREE_GRASS_DARKER", "tiles/twoD/TREE_GRASS_DARKER_TILE.png", false, false);
+	public static Tile TREE_GRASS = new Tile("TREE_GRASS", "tiles/twoD/TREE_GRASS_TILE.png", false, false);
 }
